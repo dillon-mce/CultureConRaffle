5950b08f62d4df0bf7512c6dd829307143925e7e
Dillon McElhinney <dillon.mce@gmail.com>
2021-02-09 14:42:10 -0700

    First pass solution

--- CultureConRaffle/main.swift
+++ CultureConRaffle/main.swift
@@ -5,7 +5,29 @@
 //  Created by Dillon McElhinney on 2/9/21.
 //
 
+import Algorithms
+import CryptoSwift
 import Foundation
 
-print("Hello, World!")
+let hash = "zdVljhHnF+CnhphNzQWHRQ=="
+let email = "dillon.mcelhinney@ibotta.com"
 
+let alphabet = "abcdefghijklmnopqrstuvwxyz1234567890"
+var combinations = 0
+let startTime = CFAbsoluteTimeGetCurrent()
+alphabet.map(String.init).combinations(ofCount: 5).forEach { combo in
+    combinations += 1
+    combo.permutations().forEach { permutation in
+        autoreleasepool {
+            let raffle = permutation.joined()
+            if let test = (email + raffle).bytes.md5().toBase64(),
+               test == hash {
+                print("Found it!")
+                print("The correct raffle is \(raffle)")
+            }
+        }
+    }
+    if combinations % 10000 == 0 {
+        print("Checked \(combinations) combinations - \(CFAbsoluteTimeGetCurrent() - startTime) sec")
+    }
+}
